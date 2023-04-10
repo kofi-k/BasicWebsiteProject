8e10f63a6cfd6933804e144333ee0335e1bdddb5
Kofi K <89961275+kofi-k@users.noreply.github.com>
2023-04-10 16:01:09 +0000

    modified:   register.php

---
+++ register.php
@@ -0,0 +1,15 @@
+<?php
+
+// get the data from the form
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$phone = $_POST['phone'];
+$birthday = $_POST['dob'];
+
+
+// connect to the database
+
+
+
+?>
